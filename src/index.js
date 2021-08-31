49037eb21c4f4db65842c40c4c55ab89a1ab16e1
RobiulMan <robiulislam.bg@gmail.com>
2021-09-01 01:08:26 +0600

    Create an express server
    feat: installed devdepandencies aslo dependencies express, dotenv settup express server

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app = express()
+const PORT = 3010 || process.env.PORT;
+
+app.get('/', (req, res) => {
+    res.status(200).send({
+        hello: 'world'
+    })
+})
+app.listen(PORT, () => {
+    console.log('server is running...')
+})
