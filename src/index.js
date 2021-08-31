4f2e4f49a76b6be457359f080c03bbfad1eeec5f
RobiulMan <robiulislam.bg@gmail.com>
2021-09-01 02:18:56 +0600

    Scaffold an express application
    fate: set cors morgan static file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 const express = require('express')
-
+const cors = require('cors')
+const path = require('path')
+const morgan = require('morgan')
 const app = express()
 const PORT = 3010 || process.env.PORT;
-app.use(express.json())
+
+app.use(morgan('dev'))
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../', 'public')));
+
+
+
 app.get('/', (req, res) => {
     res.status(200).send({
         hello: 'world'
