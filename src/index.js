2efeab58deaaa0ad8ae252d0b695e56883f588f2
RobiulMan <robiulislam.bg@gmail.com>
2021-09-01 01:58:35 +0600

    added express perser

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const app = express()
 const PORT = 3010 || process.env.PORT;
-
+app.use(express.json())
 app.get('/', (req, res) => {
     res.status(200).send({
         hello: 'world'
