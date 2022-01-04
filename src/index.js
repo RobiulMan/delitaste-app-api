4e772b33cf67a9aa9e84b84ba4eca6de3ba2245a
RobiulMan <robiulislam.bg@gmail.com>
2022-01-04 14:10:56 +0600

    Implement Database Models Schema
    Configered the Mongoose with connection and database collection name is: delitaste

--- src/index.js
+++ src/index.js
@@ -2,8 +2,15 @@ const express = require('express')
 const cors = require('cors')
 const path = require('path')
 const morgan = require('morgan')
+const dbConnection = require('./config/db.js');
+
+
+
 const app = express()
-const PORT = 3010 || process.env.PORT;
+const PORT = 3010;
+
+// Mongodb connection
+dbConnection();
 
 app.use(morgan('dev'))
 app.use(cors());
@@ -11,13 +18,33 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../', 'public')));
 
-
-
 app.get('/', (req, res) => {
     res.status(200).send({
         hello: 'world'
     })
 })
+
+
+app.use((req, res, next) => {
+    const error = new Error('404 Page Not Found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    if (error.status === 404) {
+        res.status(404).json({
+            error
+        });
+    } else {
+        res.status(500).json({
+            error: '500'
+        });
+    }
+    next();
+});
+
+
 app.listen(PORT, () => {
     console.log('server is running...')
 })
